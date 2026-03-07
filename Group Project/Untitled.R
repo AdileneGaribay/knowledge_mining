aa85584ae8dd857d4229a1fd19a569f5e9e3af4d
Adilene Garibay <adilenegaribay@Adilenes-MacBook-Pro.local>
2026-03-06 18:28:38 -0600

    fix presentation PDF link

--- Group Project/Untitled.R
+++
@@ -1,9 +0,0 @@
-install.packages(c("httr2", "jsonlite", "dplyr", "stringr", "base64enc", "readr"))
-library(httr2)
-library(jsonlite)
-library(dplyr)
-library(stringr)
-library(base64enc)
-library(readr)
-# Function to get the access token
-usethis::edit_r_environ()
